6ded731be4c594972520861e517e47e0f8753cd1
郝 <961406169@qq.com>
2020-10-04 22:39:07 +0800

    添加的文件

---
+++ src/views/category/category.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>分类</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "category",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+</style>
