0d1fe29c75854e2db202d6c070e6298f43d497cd
郝 <961406169@qq.com>
2020-10-05 23:48:34 +0800

    轮播图部分

---
+++ src/views/home/childComps/HywRecommend.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommend" :key="item.id" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+      </a>
+      <div>{{ item.title }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HywRecommend",
+  props: {
+    recommend: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+  
+<style scoped>
+.recommend {
+  display: flex;
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+  padding: 12px 0 18px 0;
+  border-bottom: 15px solid #eee;
+}
+.recommend-item {
+  flex: 1;
+}
+.recommend-item img {
+  width: 70px;
+  height: 70px;
+  margin-bottom: 5px;
+}
+</style>
